f13f1bf7b49186afc017cbd4926996af0a1493a5
Bene <bene@peernova.com>
2025-04-02 17:19:39 -0700

    fixing build on OSX version prior 15.4

--- parser/include/postgres/pg_config.h
+++ parser/include/postgres/pg_config.h
@@ -401,8 +401,7 @@
 /* #undef HAVE_STRCHRNUL */
 #if __APPLE__
 #include <AvailabilityMacros.h>
-#if defined(MAC_OS_X_VERSION_MIN_REQUIRED) && defined(MAC_OS_X_VERSION_15_4)
-#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_15_4
+#if defined(MAC_OS_X_VERSION_15_4)
     #define HAVE_STRCHRNUL 1
 #endif
 #endif
